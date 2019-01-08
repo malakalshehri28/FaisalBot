57e2c528a9a028d558d4ed7638f72e31d1965de4
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-01-08 19:52:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,7 +67,7 @@ client.on('message', async msg => { // eslint-disable-line
 			        .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 //by ! - .'
-					.setFooter("AJ Server")
+					.setFooter("Des1ko")
 					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
 					
 					// eslint-disable-next-line max-depth
