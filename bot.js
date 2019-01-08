a29a697e866473b4e1869699e7f4aea7ce18f264
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-01-08 19:49:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -285,31 +285,4 @@ if (message.content.startsWith(PREFIX + 'sp')) {
 
 
 
-
-
-
-const adminprefix = "D";
-const devs = ['532188566978166804'];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-    
-  if (message.content.startsWith(adminprefix + 'setname')) {
-client.user.setUsername(argresult).then;
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`);
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'setavatar')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else     
-
-});
-	
-	
-	
-	
-	
-	
-	
 client.login(process.env.BOT_TOKEN);
