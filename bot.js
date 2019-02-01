47eaddc4d3dd2b834dc03ac60ba351125cdbe01a
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-02-01 18:46:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -210,26 +210,31 @@ client.on('message', message => {
 
 
 
-
-client.on('message', (message) => {
-
-    if (message.content.startsWith('kick')) {
-
-        var member= message.mentions.members.first();
-
-        member.kick().then((member) => {
-
-            message.channel.send(member.displayName + ' لقد تم اعطاء كيك الى الشخص بنجاح');
-
-        }).catch(() => {
-
-            message.channel.send(":x:");
-
-        });
-
-    }
-
+client.on('message', message => {
+  var prefix = "#";
+  if (message.author.omar) return;
+  if (!message.content.startsWith(prefix)) return;
+  var command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  var args = message.content.split(" ").slice(1);
+  if (command == "kick") {
+   if(!message.channel.guild) return message.reply('** This command only for servers :x:**');
+   const guild = message.guild;
+  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**I Don't Have ` KICK_MEMBERS ` Permission**");
+  var user = message.mentions.users.first();
+  var reason = message.content.split(" ").slice(2).join(" ");
+  if (message.mentions.users.size < 1) return message.reply("**__Mention__ A Member To Kick !**");
+  if (!message.guild.member(user).kickable) return message.reply("**Can't Kick A Higher Role Than Me !**");
+  message.channel.send(`**:white_check_mark: ${user.tag} Kicked Form The Server By : <@${message.author.id}> ! :airplane:** `)
+  guild.owner.send(`سيرفر : ${guild.name}
+**تم طرد** :${user.tag}  
+**بواسطة** : <@${message.author.id}>`).then(()=>{
+message.guild.member(user).kick();
+  })
+}
 });
+
 
 
 
