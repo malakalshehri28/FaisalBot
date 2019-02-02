e0b91b2a4d38d7634ced4c5817e0bbcc8cd5fd2c
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-02-02 12:59:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -288,7 +288,7 @@ const prefix = '#'
  
  
   client.on('message', msg => {//msg
-    if (msg.content === ('الوان') {
+    if (msg.content === ('colors') {
       msg.channel.send({file : "https://cdn.pg.sa/1c4R2LijPA.png"})
     }
   });
@@ -302,7 +302,7 @@ const prefix = '#'
 
 client.on('message', message => {
             let args = message.content.split(' ').slice(1);
-            if(message.content.split(' ')[0] == `${prefix}لون`){
+            if(message.content.split(' ')[0] == `${prefix}color`){
             const embedd = new Discord.RichEmbed()
             .setFooter('Requested by '+message.author.username, message.author.avatarURL)
             .setDescription(`**لا يوجد لون بهذا الأسم ** ❌ `)
