defd60796b3957b040a5b3613676c92b0fdbafdf
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-02-18 18:54:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ client.on('guildMemberAdd', member => {
     invites[member.guild.id] = guildInvites;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const logChannel = member.guild.channels.find(channel => channel.name === "welcome");
+    const logChannel = member.guild.channels.find(channel => channel.name === "all");
     logChannel.send(`invite by: <@${inviter.id}>`);
   });
 });
