b90b91642c552a0153865e62b0b64536d2bcfd5b
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-02-18 18:14:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,17 @@ client.user.setGame(`Free Mic.`,"http://twitch.tv//idk")
 
 
 
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const xkiller = member.guild.channels.find("name", "welcome");
+     xkiller.send(`invite by <@${inviter.id}>`);
+  });
+});
+
+
 
 client.on("message", message => {
  
