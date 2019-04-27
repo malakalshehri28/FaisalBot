595c0f58dc49f89bfc92bab4c7816b821e8754b7
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-04-28 02:31:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ client.on('guildMemberAdd', member => {
     invites[member.guild.id] = guildInvites;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const logChannel = member.guild.channels.find(channel => channel.name === "txt");
+    const logChannel = member.guild.channels.find(channel => channel.name === "chat");
     logChannel.send(`**Welcome to Server FreeMic.**
 invite by: <@${inviter.id}>`);
   });
