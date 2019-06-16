63a102d0ca33faf2e100e52135c9f590b8b33d94
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-06-17 02:27:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,13 +2,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs")
 var prefix = "#";
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : Badstation `);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : Badstation ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
+
+
 client.user.setActivity('BadStation.', { type: 'WATCHING' })
 });
 
